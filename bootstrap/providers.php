2380f844e590e5357bf8592eaae39d1df20dd1b5
tajciglar <taj.brzovic@gmail.com>
2026-04-21 14:51:58 +0200

    feat(media-library): register bunnycdn Storage driver

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\BunnyStorageServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
 
 return [
     AppServiceProvider::class,
+    BunnyStorageServiceProvider::class,
     AdminPanelProvider::class,
 ];
